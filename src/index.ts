1ad5983c2a482f2a85ca5bbb31eb4b72b9a24e1d
Javier Diaz <diazjavierdavid@hotmail.com>
2021-06-25 14:51:36 -0300

    se agrega timer, boton play, boton stop

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-// Abrir y cerrar modal
-const closeBtn = document.querySelector('.modal__close img') as HTMLImageElement;
+const clockTime = document.querySelector('.clock__time') as HTMLParagraphElement;
+const endTime = document.querySelector('.clock__endtime') as HTMLParagraphElement;
 const modal = document.querySelector('.modal__wrapper') as HTMLDivElement;
+const closeBtn = document.querySelector('.modal__close img') as HTMLImageElement;
 const optionsBtn = document.querySelector('.btn__options') as HTMLButtonElement;
-const minutesDom = document.querySelector('.minutes') as HTMLSpanElement;
-const secondsDom = document.querySelector('.seconds') as HTMLSpanElement;
-const startBtn = document.querySelector('.btn_start') as HTMLButtonElement;
+const startBtn = document.querySelector('.btn__start') as HTMLButtonElement;
+const stopBtn = document.querySelector('.btn__stop') as HTMLButtonElement;
+const form = document.querySelector('form') as HTMLFormElement;
 
+
+// Abrir y cerrar modal
 optionsBtn.addEventListener('click', () => {
     modal.classList.add('open');
 });
@@ -15,40 +18,89 @@ closeBtn.addEventListener('click', () => {
 });
 
 // pomodoro 
+let countdown: NodeJS.Timeout;
 
-const pomodoro = {
+//Muestra tiempo restante
+const displayTimeLeft = (seconds: number) => {
+    if(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
+    clockTime.textContent = display;
+    document.title = (display === "0:00") ? "Pomodoro Finalizado" : display;}
+    else return;
+};
+
+// Muestra hora de finalización
+const displayEndTime = (timestamp: number) => {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Próximo descanso - ${hour}:${minutes < 10 ? "0" : ""}${minutes}`;
+}
+
+interface isPomodoro {
+    started: boolean;
+    time: number;
+    timer(): void;
+}
+
+const pomodoro: isPomodoro = {
        started: false,
-       minutes: 25,
-       seconds: 0,
-       //setea los minutos del pomodoro
-       setPomodoroTime(minutes: number = 25): void {
-           this.minutes = Math.abs(minutes);
-           this.minutes < 10 && this.minutes > -1
-           ? minutesDom.innerText = "0" + this.minutes.toFixed().toString()
-           : minutesDom.innerText = this.minutes.toFixed().toString();
-           secondsDom.innerText = "0" + this.seconds.toString();
-       },
-       // 
-       refreshPomodoro():void {
-           if(this.minutes != 0) {
-                this.seconds = 60;
-                setInterval(() => {
-                    const newTime = this.seconds -1;
-                    this.setPomodoroTime(newTime);
-                }, 1000);
-           }
+       time: 25,
+       timer(){
+           //limpia los timers existentes
+            clearInterval(countdown);
+
+           const now = Date.now();
+           const then = now + (this.time * 60) * 1000;
+           displayTimeLeft(this.time*60);
+           displayEndTime(then);
+
+           countdown = setInterval(() => {
+            const secondsLeft = Math.round((then - Date.now()) / 1000);
+            // Se detiene al llegar a 0
+            if(secondsLeft < 0){
+                clearInterval(countdown);
+                return;
+            }
+            //display
+            displayTimeLeft(secondsLeft);
+           }, 1000);
        }
 }
 
-window.onload = () => {
-    pomodoro.setPomodoroTime();
-    pomodoro.refreshPomodoro();
+window.onload= () => {displayTimeLeft(25*60)};
+
+const formData = (form: HTMLFormElement) => {
+    const inputs = form.querySelectorAll('input');
+    let values: {[prop: string]: string} = {};
+
+    inputs.forEach(input => {
+        values[input.id] = input.value;
+    });
+    return values;
 };
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    clearInterval(countdown);
+    const data = formData(form);
+    const pomoMinutes = parseInt(data.datapomodoro);
+    const breakMinutes = parseInt(data.databreak);
+    pomodoro.time = pomoMinutes;
+    displayTimeLeft(pomoMinutes*60);
+    modal.classList.remove('open');
+    form.reset();
+    endTime.textContent = "";
+});
 
+startBtn.addEventListener('click', () => {
+    pomodoro.timer();
+});
 
-const play = (): void => {
-    setInterval(() => {
-
-    }, 1000)
-}
+stopBtn.addEventListener('click', () => {
+    clearInterval(countdown);
+    console.log("detener");
+    endTime.textContent = "";
+});
